b74a20b24ec75d1948203ca8d26d6901c7365c4b
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-19 08:19:39 +0000

    Datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,5 +2,35 @@
 
 // alert(3+3);     // this will give error as we are working in node, not in browser
 
-console.log(3+3);
-console.log("Shivam");
+// console.log(3+3);
+// console.log("Shivam");
+
+
+let name = "shivam";
+/* 
+
+DATATYPES IN JAVASCRIPT:
+
+    • PRIMITIVE DATA TYPES: 
+
+    NUMBER - 2^53 - 1;
+    BIGINT      // IF VALUE EXCEEDS THE NUMBNER RANGE(2^53-1) 
+    BOOLEAN - true/false;
+
+    null - standalone value     // it is a representation of an empty value.
+    eg . let name = null;  // this name variable contains null
+    
+    undefined       // value which is not defined
+    eg. let age;    // this is an example of undefined behaviour
+
+    symbol - to find the uniqueness when we are working on multiple things
+
+    • NON PREMITIVE DATATYPES:
+    object
+
+
+    on checking the type of any datatype the typeof null is an object
+    and typeof undefined is undefined.
+*/
+// console.log(typeof null);            // object
+// console.log(typeof undefined);       // undefined
