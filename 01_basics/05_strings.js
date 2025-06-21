248f33cbce2b26115afa07e7de58af641ab3a2a5
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-21 16:53:38 +0000

    Strings basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+// const name = "Shivam";
+// const repoCount = 12;
+// console.log(name + repoCount + " Value");   // not recommended 
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Shivam-Pahuja-SP");
+
+// console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('S'));
+// const newString = gameName.substring(2, 6);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+// const newStringOne = "      Shivam  Pahuja    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());   // remove the trailing or leading spaces
+
+
+
+// const url = "https://shivampahuja1212.com/shivam%20pahuja%20how%20are%20you";
+// console.log(url.replace("%20", "-"));
+
+
+// console.log(gameName.split('-'));   // [ 'Shivam', 'Pahuja', 'SP' ]
+
+
+
+/* 
+    do more and more string methods...
+*/
