b6143956c10bd931a1641fd2bbc95848e6031939
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-21 07:32:34 +0000

    conversion updated !

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -53,7 +53,26 @@
 // let s3 = s1 + s2;        // to concatenate two strings
 // console.log(s3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" +2 + 2);
-console.log(1 +2 + "2");
+// console.log("1" + 2);   // 12
+// console.log(1 + "2");   // 12
+// console.log("1" +2 + 2);    // in this operation 1 is a string therefore it there the other operations as string and concatentate them all = 122.
+// console.log(1 +2 + "2");    // but in this example the first operation is addition therefore the result is the completion of first operation which is addition and then concatenation of string occurs = 32
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+// console.log(""+); // error
+
+
+// let n1, n2, n3;
+// n1 = n2 = n3 = 4;
+// console.table([n1, n2, n3]);
+
+// let counter = 100;
+// let ans1 = counter++;    // post increment
+// let ans2 = ++counter;    // pre increment
+// console.log(ans1); // 100
+// console.log(ans2); // 102
+
+
