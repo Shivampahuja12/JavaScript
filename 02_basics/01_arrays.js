852f746c40ef99244c124f244482e62256fd8edc
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-22 17:55:03 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+const myArr = [1,2,3,4,5, "hello", true];
+// myArr[myArr.length] = "shivam";
+// myArr[myArr.length] = 1230;
+// myArr[myArr.length] = "pahuja";
+// myArr[myArr.length] = "shivam";
+// console.table(myArr);
+
+const arr = new Array(1,2,3,4);
+// console.log(arr);
+
+
+// array methods
+
+// arr.push(5);
+// arr.pop();
+// arr.unshift(9);     // insert element at the starting of the array.
+// arr.shift();   // removes element from the starting of the array.
+// console.log(arr.includes(9));
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(9));
+// console.log(arr.indexOf(3));
+
+
+// const newArr = arr.join();  // join converted array to a string and assigned it to the variable newArr;
+
+// console.log(arr);
+// console.log(newArr);
+// console.log(typeof arr);    // object
+// console.log(typeof newArr);     // string
+
+
+
+// slice, splice ** IMPORTANT DO IT AGAIN
+// console.log("A", arr);
+// const myn1 = arr.slice(1, 3);
+// const myn2 = arr.splice(1, 3);
+// console.log(myn1);  
+// console.log("B", arr);
+// console.log(myn2);  
