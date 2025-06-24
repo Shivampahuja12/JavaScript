2d95b194ab5e1960bb265595bf20eebabb8588d2
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-24 17:44:08 +0000

    More on arrays !

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,34 @@
+// more methods 
+// const a = [5,3,2,5,1,6];
+// console.log(a.sort());
+
+
+const bhai = ["shivam", "harsh", "manan", "aman", "hemaksh"];
+const moreBhai = ["addy", "abhishek", "ankush", "tashu", "sunny"];
+
+// bhai.push(moreBhai);
+// console.log(bhai);
+
+// const allBhai = bhai.concat(moreBhai);
+// // console.log(allBhai);
+// console.table(allBhai);
+
+// const newAll = [...bhai, ...moreBhai];   // spread operator, multiple arrays can be concatenated.
+// console.log(newAll);
+
+// const a = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const arr = a.flat(Infinity);
+// console.log(arr);
+
+// console.log(Array.isArray("Shivam"));
+// console.log(Array.from("Shivam"));
+// console.log(Array.from({name: "Shivam"}));  // here this object is empty because we have to define that if we want to make array of keys or value.
+
+
+// let s1 = 100;
+// let s2 = 200;
+// let s3 = 300;
+// console.log(Array.of(s1, s2, s3));
+
+
+// do more array methods 
