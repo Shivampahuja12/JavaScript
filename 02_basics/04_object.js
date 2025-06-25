bff68122b12fc3bb2956b7e9748996ef7dc835b4
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-25 20:32:10 +0000

    More on objects

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -22,5 +22,33 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "a", 4: "b"};
 
-const obj3 = {obj1, obj2};
-console.log(obj3);
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({},obj1, obj2);  // target and source
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+// when entries come from the database.
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinder);
+
+console.log(Object.keys(tinder));
+console.log(Object.values(tinder));
+console.log(Object.entries(tinder));
+
+console.log(tinder.hasOwnProperty('isLoggedIn'));
